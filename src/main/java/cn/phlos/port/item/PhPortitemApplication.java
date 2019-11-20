3b2afae65e05003ea3a6eb2885782b62dba7cf47
lphx <li15775063262>
2019-11-20 16:05:41 +0800

    提交项目，进行开发

---
+++ src/main/java/cn/phlos/port/item/PhPortitemApplication.java
@@ -0,0 +1,13 @@
+package cn.phlos.port.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhPortitemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhPortitemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/phlos/port/item/PhPortitemApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.phlos.port.item;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhPortitemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
